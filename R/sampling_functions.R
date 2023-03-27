7684f88e5e98f2ad9e3446bde999039b4122ce86
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-27 17:20:33 +0100

    changes to sampling practical

---
+++ R/sampling_functions.R
@@ -0,0 +1,104 @@
+
+#' @title SIR Step Functions
+#' @description
+#' Provides a step function for a *S*usceptible-*I*nfectious-*R*emoved
+#' ODE model, using a beta-gamma parameterization, complying with the
+#' [deSolve::ode()] `func` argument requirements.
+#'
+#' @param t time derivatives to be evaluated (ignored in this model)
+#'
+#' @param state a named, numeric vector, c(S=.., I=..., R=...); the current
+#' system state
+#'
+#' @param parms a list of parameters, containing `beta` and `gamma` values
+#' (for `sampling_dSIR_betagamma`) or `gamma` and `R0` values
+#' (for `sampling_dSIR_gammaR0`).
+#' All parameters must be positive, scalar values.
+#'
+#' @return a list, complying with the [deSolve::ode()] `func` return
+#' requirement
+#'
+#' @examples
+#' require(MTM)
+#' require(deSolve)
+#' pars <- list(beta = 0.4, gamma = 0.2, R0 = 2)
+#' bg <- ode(y = c(S=99, I=1, R=0), times = 0:50, func = sampling_dSIR_betagamma, parms = pars, method = "rk4")
+#' gR <- ode(y = c(S=99, I=1, R=0), times = 0:50, func = sampling_dSIR_gammaR0, parms = pars, method = "rk4")
+#' @rdname sampling_models
+#' @export
+#' @family sampling
+sampling_dSIR_betagamma <- function(t, state, parms) with(parms, {
+  # compute total population size
+  N <- sum(state)
+
+  # define processes
+  infection <- (beta * state["S"] * state["I"]) / N
+  recovery <- gamma * state["I"]
+
+  # return according to deSolve::ode requirements:
+  # list, first element the derivatives, same order as state
+  return(list(
+    c(dS = -infection, dI = infection - recovery, dR = recovery)
+  ))
+})
+
+#' @rdname sampling_models
+#' @export
+sampling_dSIR_gammaR0 <- function(times, state, parms) sampling_dSIR_betagamma(
+  times, state, within(parms, { beta = gamma*R0 })
+)
+
+#' @title Find Max Prevalence in SIR ODE
+#' @description
+#' Given appropriate parameter values, solves an SIR model,
+#' then finds the maximum "I" value. Uses fixed population
+#' of c(S=99, I=1, R=0), and runs out to `t=50`.
+#'
+#' @param parameters a list of the parameters appropriate to use with `dModel`
+#'
+#' @param dModel either [sampling_dSIR_betagamma()] or [sampling_dSIR_gammaR0()]
+#'
+#' @param plot_results plot (to the active device) the "S" and "I" compartments
+#'
+#' @return the maximum "I" value for the solved ODE, a numeric scalar
+#' @examples
+#' require(MTM)
+#' print(sampling_maxprevalence(ist(beta = 0.4, gamma = 0.2)))
+#'
+#' @export
+#' @family sampling
+sampling_maxprevalence <- function(
+  parameters, dModel = sampling_dSIR_betagamma,
+  plot_results = FALSE
+) {
+
+  # Define time to solve equations
+  times <- seq(from = 0, to = 50, by = 1)
+
+  # Define initial conditions
+  N <- 100
+  I_0 <- 1
+  S_0 <- N - I_0
+  R_0 <- 0
+  state <- c( S = S_0, I = I_0, R = R_0)
+
+  # solve equations ...
+  output <- ode(
+    y = state, times = times, func = dModel, parms = parameters,
+    method = "rk4"
+  ) |> as.data.table() # ... then convert for easy extraction of columns
+
+  if(plot_results){
+    par(new=TRUE)
+    par(mfrow = c(1, 1))
+    with(output, {
+      plot(time, S, type = "l", col = 4, lwd = 2, ylim = c(0, N),
+           xlab = "Time", ylab = "Number of People", main = "")
+      lines(time, I, lwd = 2, col = 2, type = "l")
+      legend("topright", legend = c("Susceptible", "Infected"),
+             lty = rep(1, 2), col = c(4, 2), lwd = 2, bty = "n")
+    })
+  }
+
+  return(max(output$I))
+}
--- inst/scripts/sampling/Practical_06.R
+++ inst/scripts/sampling/Practical_06.R
@@ -2,81 +2,76 @@
 ### Practical 10 - Sensitivity and Sampling ###
 
 ########################### (1) ONE-WAY UNCERTAINTY ANALYSIS ##############################
-# First, let's run an ODE model.
-# Open up the SIRmodel.R file
-
-# (a) Which functions are in here?
-# Answer: 
-# (b) What are the arguments of the first function?
-# Answer: 
-# (c) What is the output of the first function?
-# Answer: 
-
 # First let's clear our workspace, remove plots and load the libraries we need
 rm(list=ls())
 dev.off()
+library(MTM)
 library(deSolve)
 
-# Let's read in these functions so we have them to hand
-#### <YOUR CODE HERE> #### 
+# Examine the pertinent functions for this step of practical
+sampling_dSIR_betagamma
+sampling_maxprevalence
+
+# (a) What is the first function?
+# Answer: a step function for the SIR model, parametrised by beta and gamma,
+# (b) What are the arguments of the second function?
+# Answer: (1) the parameter values lists, (2) whether or not to plot results, (3) which model to use
+# (c) What is the output of the second function?
+# Answer: maximum prevalence through the epidemic
 
 # Let's choose a beta value of 0.4 and a gamma value of 0.2
-max.prevalence <- solveODE(parameters <- c(beta = 0.4, gamma = 0.2))
+max.prevalence = sampling_maxprevalence(parameters = list(beta = 0.4, gamma = 0.2), plot_results = TRUE)
 print(max.prevalence)
 
 # Now let's look at the effect of the maximum prevalence of the epidemic across gamma = 0.1 -1.0 (increment on 0.1)
-
 gamma.vec <- #### <YOUR CODE HERE> ####
-  
-# initialise max/prevalence container  
-max.prevalence <- vector()
+# initialise max.prevalence container (to the same size as gamma.vec)
+max.prevalence = numeric(length(gamma.vec))
 
-# Add in a loop to make this happen
+# Wrap this in a loop to fill in the max.prevalence vector
 #### <YOUR CODE HERE> ####
-  
-  mp <- solveODE(parameters = c(beta = 0.4, gamma = gamma.val),
-                                         plot.all.results = FALSE)
-  max.prevalence <- c(max.prevalence, mp)
-  
-  #### <YOUR CODE HERE> ####
+  max.prevalence[i] = sampling_maxprevalence(
+    parameters = list(beta = 0.4, gamma = gamma.vec[i])
+  )
+#### <YOUR CODE HERE> ####
 
 # Now we have our max.prevalence, we need to plot this against our infectiousness duration
 
 # plot max.prevalence as a function of the *infectiousness duration*
 par(new=FALSE)
 par(mfrow=c(1,1))
-plot(#### <YOUR CODE HERE> ####, #### <YOUR CODE HERE> ####, type = "b", 
+plot(#### <YOUR CODE HERE> ####, #### <YOUR CODE HERE> ####, type = "b",
      xlab = "Infectiousness Duration (days)",
      ylab = "Maximum Prevalence",
      main= "One-way uncertainty analysis")
 
-
 ## Now try to increase the resolution of gamma to get a better idea of the relationship
 # but remember to clear max.prevalence first!
 
-
 #### <YOUR CODE HERE> ####
-  
+
+
 
 
 # (d) Describe in words the qualitative relationship
-# Answer: 
+# Answer:
 
 
 
 ###################### (2) MONTE CARLO SAMPLING ######################
 
-# Now suppose that we have a previous epidemiological study that suggested that R0 has a mean value of 5, 
+# Now suppose that we have a previous epidemiological study that suggested that R0 has a mean value of 5,
 # but uncertainty within the range of -1, +1
-# However, we still don't know whether the infectiousness period is 1 day or 10 days. 
-# We will now use the functions in SIRmodel_R0.R to make a similar plot as above, but this time, 
+# However, we still don't know whether the infectiousness period is 1 day or 10 days.
+# We will now use the functions in SIRmodel_R0.R to make a similar plot as above, but this time,
 # incorporate the uncertainty of R0 for each discrete value of gamma
 
 # We're going to first use a direct Monte Carlo Sampling method
+# Let's look at the different model step function we'll be using:
+sampling_dSIR_gammaR0
+# note: we're using the relationship between R0 and beta here,
+# then re-using the sampling_dSIR_betagamma function
 
-# Read in our set of functions in SIRmodel_R0.r
-#### <YOUR CODE HERE> ####
-  
 # First, let's set a fixed seed for the random number generator
 # this will allow us to run the code again and retrieve the same 'simulation'
 
@@ -86,40 +81,41 @@ set.seed(2019)
 r0.all = #### <YOUR CODE HERE> ####
 
 # initialise max.prevalence again, this time it needs to be a dataframe or a matrix
-size.df = length(r0.all) * length(gamma.vec)
-max.prevalence = data.frame(r0.value = vector(mode= "numeric", length = size.df), 
-                            gamma = vector(mode= "numeric", length = size.df), 
-                            max.prev = vector(mode= "numeric", length = size.df))
-index = 0
-# create a loop over each of these R0 values in turn
-for (r0.val in r0.all){
-  
-  # create a loop over each of these Gamma values in turn
-  for (gamma.val in gamma.vec){
-      index = index + 1     
-      mp = solveODE_2(parameters = c(R0 = r0.val, gamma = gamma.val))
-      max.prevalence[index, "r0.value"] = r0.val
-      max.prevalence[index, "gamma"] = gamma.val
-      max.prevalence[index, "max.prev"] = mp
-  }
+# `expand.grid` is convenient function to give you all combinations of some values
+# as a `data.frame`
+max.prevalence <- expand.grid(r0.value = r0.all, gamma = gamma.vec)
+# now add a column to-be-filled-in
+max.prevalence$max.prev <- NA_real_
+
+# create a loop over all the samples, and evaluate
+# the max prevalence at each
+# n.b. this will take a minute
+for (sample_index in 1:dim(max.prevalence)[1]) {
+  max.prevalence$max.prev[sample_index] <- sampling_maxprevalence(
+    parameters = list(
+      R0 = max.prevalence$r0.value[sample_index],
+      gamma = max.prevalence$gamma[sample_index]
+    ),
+    dModel = sampling_dSIR_gammaR0
+  )
 }
 
 # Take a look at max.prevalence by using the 'head() function
 head(max.prevalence)
 # (e) How have we saved the output?
-# Answer: 
+# Answer:
 
-# Now plot this output using the R function MCplot() in SIRmodel_R0.R
-MCplot(max.prevalence)
+# Now plot this output using the R function MTM::sampling_MC_plot()
+sampling_MC_plot(max.prevalence)
 
 # (f) What conclusions can you draw from the plot?
-# Answer: 
+# Answer:
 
 # (g) Why does the plot look different to the one you plotted previously?
 
-# Final exercise:  We have used a combination of scripts and function to complete this plot. 
+# Final exercise:  We have used a combination of scripts and function to complete this plot.
 # (g) Write out the description of how we could plot this output ONLY using functions (optimal)
-# Answer: 
+# Answer:
 
-# (h) Translate this outline to actual code - notice how there is a trade-off between making your code 
+# (h) Translate this outline to actual code - notice how there is a trade-off between making your code
  # quick to write and making it generalisable to other situations
