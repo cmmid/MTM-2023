c82e02aa7989d02d40b5b378cd795b988bffaac5
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-29 09:49:02 +0100

    working solution for 01 practical in "new" format

--- inst/solutions/10_StochasticContinuous/Practical_01.R
+++ inst/solutions/10_StochasticContinuous/Practical_01.R
@@ -1,11 +1,14 @@
-library('ggplot2') ## for plotting
-library('data.table') ## for manipulation of results
+library('ggplot2')    # for plotting
+library('data.table') # for manipulation of results
+library('MTM')        # for definitions from earlier sessions
+library('deSolve')    # for comparison to ODE model results
 
 #' @section Overview
+#'
 #' In this practical session, we will simulate the SIR model using
 #' *Gillespie's algorithm*. Take ~10 minutes for this section to review how the
 #' algorithm works in practice.
-
+#'
 #' Recall from the discussion: the Gillespie algorithm is about *events* and
 #' *rates*. In the SIR model, there are two events:
 
@@ -14,7 +17,7 @@ SIR_events <- list(
   recovery = c(I = -1, R = +1)
 )
 
-#' and the associated rates, in terms of a `state = list(S=..., I=..., R=...)`
+#' and the associated rates, in terms of a `state = c(S=..., I=..., R=...)`
 #' and `parms = list(beta = ..., gamma = ...)` (and `time`, which is ignored
 #' in the minimal SIR model):
 
@@ -23,7 +26,8 @@ SIR_rates <- function(time, state, parms) with(c(parms, as.list(state)), {
   return(c(infection = beta * S * I/N, recovery = gamma * I))
 })
 
-#' Then in terms of Gillespie's algorithm, a solver is
+#' Then in terms of Gillespie's algorithm, a solver is:
+
 stochcont_solve <- function(init_state, transitions, rateFun, params, tf) {
   time <- 0 ## initialise time to 0
   results_df <- list(as.list(c(time=0, init_state)))
@@ -47,11 +51,14 @@ stochcont_solve <- function(init_state, transitions, rateFun, params, tf) {
       time <- tf
     }
     ## add new row to results data frame
-    results_df[[length(results_df)+1]] <- list(time=time, x)
+    results_df[[length(results_df)+1]] <- as.list(c(time=time, x))
   }
   ## return results data frame
   return(rbindlist(results_df))
 }
+
+#' To use the solver, we apply it to some initial conditions, parameters
+#' and the model-defining transitions and rates
 
 init.values <- c(S=249, I=1, R=0) ## initial state
 parms <- list(beta=1, gamma=0.5) ## parameter vector
@@ -59,127 +66,158 @@ tmax <- 20 ## end time
 
 r <- stochcont_solve(init.values, SIR_events, SIR_rates, parms, tmax)
 
-## run Gillespie simulation
-# r <- SIR_gillespie(init_state=init.values, parms=parms, tf=tmax)
+#' @section Visualisation
+#'
+#' @question Now, plot the result (using [ggplot2::ggplot()] or [plot()]).
+#' @answer
 
-## Now, plot the result (using ggplot or plot)
-
-## first, convert the result into a long data frame for plotting
+# convert the result into long format
 lr <- r |> melt.data.table(
   id.vars = "time", variable.name = "compartment", value.name = "count"
 )
 
-## plot the result
+# plot the result
 ggplot(lr, aes(x=time, y=count, colour=compartment)) +
   geom_line()
 
-## re-run this a few times to convince yourself the output is different every time
+#' @question Repeat the above steps (run => plot) a few times - are the results
+#' different every time?
+#' @answer Yes, though it may be hard to tell for some.
+#'
+#' @section Sampling
+#'
+#' Let's run many simulations, gather them, and plot them to get a better idea
+#' of how different trajectories can be. We're going to run the simulation
+#' `nsim` times, then bind up all the results, with a column `sample_id`
+#' indicating which simulation results go together.
 
-## Next, run multiple simulation runs and plot a few of them
 nsim <- 100 ## number of trial simulations
 
-## We store the simulations in a data frame, traj, which
-## contains the results from multiple simulation runs and an additional column
-## that represents the simulation index
-traj <- lapply(1:nsim, \(sample_id) SIR_gillespie(init.values, parms, tmax)) |>
-  rbindlist(idcol = "sample_id")
+traj <- lapply( # for all of ...
+  1:nsim,       # samples 1 -> nsim, run the Gillespie solver function
+  \(sample_id) stochcont_solve(init.values, SIR_events, SIR_rates, parms, tmax)
+) |> rbindlist(idcol = "sample_id") # ...and the bind the results, id'd by their sample
 
 ## convert to long data frame
 mlr <- traj |> melt.data.table(
   id.vars = c("sample_id", "time"), variable.name = "compartment", value.name = "count"
 )
 
-## Next, plot the multiple simulation runs
-## (we only plot the I compartment)
-ggplot(mlr,
-       aes(x=time, y=count, group=sample_id, color=compartment)) +
-  geom_line(alpha = 0.1) +
-  facet_wrap(~compartment)
+#' @question Now visualise all of the trajectories for the *I*nfectious compartment
+#' on a single plot.
+#' @answer
 
-## You'll notice that some outbreaks die out very quickly, while some others are
-## growing to affect large parts of the population. Let us plot the distribution of
-## overall outbreak sizes. Which proportion of outbreaks dies out quickly?
+ggplot(mlr) + aes(
+  x = time, y = count, group = sample_id, color = compartment
+) +
+  geom_line(alpha = 0.25) +
+  facet_wrap(~compartment) +
+  scale_color_discrete(guide = guide_legend(override.aes = list(alpha = 1))) +
+  theme_minimal() + theme(
+    legend.position = c(0.5, 0.95), legend.justification = c(0.5, 1)
+  )
+
+#' Some outbreaks die out very quickly, while some others are
+#' growing to affect large parts of the population.
+#'
+#' @question Plot the distribution of overall outbreak sizes.
+#' Which proportion of outbreaks dies out quickly?
+#'
+#' @answer
 
 ## create data frame of outbreak sizes
 outbreak_sizes <- mlr[
-  compartment == "R",.(size = count[which.max(time)]), by=sample_id
+  compartment == "R", .(size = count[which.max(time)]), by = sample_id
 ]
 
 ## plot it as a histogram
 ggplot(outbreak_sizes, aes(x=size)) +
-  geom_histogram()
+  geom_histogram() + theme_minimal()
 
 ## determine number of large outbreaks (defined as larger than 50)
 outbreak_sizes[size > 50, .N]
 
-## Next, calculate the mean and standard deviation of each state across the
-## multiple runs. To do that, we need the value of the different states at
-## pre-defined time steps, whereas adaptivetau only returns the times at which
-## certain events happened. In order to, for example, extract the values of the
-## trajectory at integer time points, we can use
+#' @section Outcome Distribution
+#'
+#' Next, let's calculate the mean and standard deviation of each state across the
+#' multiple runs. However, because Gillespie algorithm results may happen at
+#' different times, we need some work first. Specifically, we need the value of
+#' the different states at pre-defined time steps. In order to, for example, extract
+#' the values of the trajectory at 1/10th time points, we can use:
+
 time.points <- seq(0, tmax, by=0.1)
 timeTraj <-mlr[, .(
   time = time.points,
   count = approx(time, count, xout = time.points, method = "constant")$y
 ), by=.(sample_id, compartment)]
 
-## Now, calculate a summary trajectory containing the mean and standard
-## deviation (sd) of the number of infectious people at every time step
+#' Now, we can calculate a summary trajectory mean and standard
+#' deviation (sd) for the *I*nfectious people at our preferred time steps:
+
 sumTraj <- timeTraj[compartment == "I",.(
-  trajectories = "all", mean = mean(count), sd = sd(count)
+  compartment, trajectories = "all", mean = mean(count), sd = sd(count)
 ), by = time]
 
-## plot
-ggplot(sumTraj, aes(x=time, y=mean, ymin=pmax(0, mean-sd), ymax=mean+sd)) +
-  geom_line() +
-  geom_ribbon(alpha=0.3)
+#' Let's compare these summaries to the original trajectories, for the *I* compartment:
+ggplot() + aes(x = time, color = compartment, fill = compartment) +
+  geom_ribbon(aes(ymin = pmax(0, mean-sd), ymax = mean+sd, color = NULL), alpha = 0.2, data = sumTraj) +
+  geom_line(aes(y = count, group = sample_id), alpha = 0.25, data = mlr[compartment == "I"]) +
+  geom_line(aes(y = mean, group = NULL), data = sumTraj) +
+  theme_minimal() + theme(
+    legend.position = c(0.05, 0.95), legend.justification = c(0, 1)
+  )
 
-## As a second summary, we only consider trajectories that have not gone
-## extinct, that is where I>0
+#' @question Does it seem like the mean + envelope represents the sample trajectories lines?
+#' @answer Just considering the outbreaks that actually happen, not really!
+
+#' Now let's look at second summary, where we only consider trajectories that have not gone
+#' extinct, that is where `I > 0`
 
 sumTrajGr0 <- timeTraj[
-  (compartment=="I") & (count > 0),
-  .(trajectories = "only >0", mean = mean(count), sd = sd(count)),
+  (compartment == "I") & (count > 0),
+  .(compartment, trajectories = "only >0", mean = mean(count), sd = sd(count)),
   by=time
 ]
 
+#' and considering the same kind of comparison:
+
+ggplot() + aes(x = time, color = compartment, fill = compartment) +
+  geom_ribbon(aes(ymin = pmax(0, mean-sd), ymax = mean+sd, color = NULL), alpha = 0.2, data = sumTrajGr0) +
+  geom_line(aes(y = count, group = sample_id), alpha = 0.25, data = mlr[compartment == "I"]) +
+  geom_line(aes(y = mean, group = NULL), data = sumTrajGr0) +
+  theme_minimal() + theme(
+    legend.position = c(0.05, 0.95), legend.justification = c(0, 1)
+  )
+
+#' Let's look more closely at the difference between the with and without extinct trajectories:
+
 iTraj <- rbind(sumTraj, sumTrajGr0)
+ggplot(iTraj, aes(x = time, y = mean, ymin = pmax(0, mean-sd), ymax = mean+sd)) +
+  geom_line(aes(colour = trajectories)) +
+  geom_ribbon(aes(fill = trajectories), alpha=0.3) +
+  scale_color_brewer(palette="Set1") +
+  theme_minimal() + theme(
+    legend.position = c(0.05, 0.95), legend.justification = c(0, 1)
+  )
 
-## plot
-ggplot(iTraj, aes(x=time, y=mean, ymin=pmax(0, mean-sd), ymax=mean+sd,
-                  colour=trajectories, fill=trajectories)) +
-  geom_line() +
-  geom_ribbon(alpha=0.3) +
-  scale_color_brewer(palette="Set1")
+#' Lastly, let's look at these results compared to the deterministic SIR model from an earlier session.
+#' Recall the ODE SIR model with beta & gamma parameterization:
 
-## We now compare the two averages to the deterministic trajectory
+sampling_dSIR_betagamma
 
-## Define model function (see practical 7)
-library('deSolve')
-SIR_model <- function(times, state, parms){
-  ## Define variables
-  S <- state["S"]
-  I <- state["I"]
-  R <- state["R"]
-  N <- S + I + R
-                                        # Extract parameters
-  beta <- parms["beta"]
-  gamma <- parms["gamma"]
-                                        # Define differential equations
-  dS <- - (beta * S * I) / N
-  dI <- (beta * S * I) / N - gamma * I
-  dR <- gamma * I
-  res <- list(c(dS, dI, dR ))
-  return(res)
-}
+#' We can get the time series of this using [deSolve::ode()]:
 
-ode_output_raw <-
-  ode(y = init.values, times = seq(0, tmax), func = SIR_model, parms = parms,
-      method = "rk4")
+ode_output_raw <- ode(
+  y = init.values, times = time.points,
+  func = sampling_dSIR_betagamma,
+  parms = parms,
+  method = "rk4"
+)
+
 ## Convert to data frame for easy extraction of columns
 ode_output <- (as.data.table(ode_output_raw) |> melt.data.table(
   id.vars = "time", variable.name = "compartment", value.name = "mean"
-))[compartment == "I", .(time, trajectories = "deterministic", mean, sd = 0)]
+))[compartment == "I", .(time, compartment, trajectories = "deterministic", mean, sd = 0)]
 
 ## Combine into one big data frame
 allTraj <- rbind(
@@ -190,12 +228,15 @@ allTraj <- rbind(
 ## plot
 ggplot(allTraj, aes(x=time, y=mean, colour=trajectories)) +
   geom_line() +
-  scale_color_brewer(palette="Set1")
+  scale_color_brewer(palette="Set1") +
+  theme_minimal() + theme(
+    legend.position = c(0.05, 0.95), legend.justification = c(0, 1)
+  )
 
-## Questions:
-## 1. Compare the mean trajectory to some of the individual trajectories. In how
-## much does it represent a "typical" trajectory?
-## 2. Repeat the experiment with different values of the parameters. When does
-## deterministic vs stochastic make more or less of a difference?
-## 3. Rewrite the model code to be an SEIR model. How do the results differ?
-
+#' @question Compare the mean trajectory to some of the individual trajectories:
+#' In how much does it represent a "typical" trajectory?
+#'
+#' @question Repeat the experiment with different values of the parameters. When does
+#' deterministic vs stochastic make more or less of a difference?
+#'
+#' @question Rewrite the model code to be an SEIR model. How do the results differ?
