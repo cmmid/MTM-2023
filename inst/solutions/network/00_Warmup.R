dcae60555495881ddd28419ba1bb0b9853950519
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-31 18:30:06 +0100

    WIP

--- inst/solutions/network/00_Warmup.R
+++ inst/solutions/network/00_Warmup.R
@@ -1,228 +1,188 @@
-###############################################################################
-#                     Introduction to iGraph: Practical 0                     #
-###############################################################################
 
-while (!require(igraph)) install.packages("igraph")
+require(MTM)        # for overall course functions
+require(igraph)     # for the network capabilities
+require(ggplot2)    # for plotting
 
-########## 1 - Creating iGraph objects  #######################################
-# igraph is built around working with igraph objects, which are typically built
-# from "edgelists" or "adjacency matrices".
-#
-# An edge list is a N x 2 matrix, where each row-pair corresponds to an edge
-?graph_from_edgelist
+# you may not even need these, but they could be
+# useful if you want to do some adhoc plotting
+require(patchwork)  # for combining plots
 
-# make an edge list - we're (r=row)binding together a series of vertex pairs
-el <- rbind(c(1,2),c(1,3),c(2,3),c(3,4))
-# use it to make an igraph object
-ig <- graph_from_edgelist(el, directed = FALSE)
-# view that object
-plot(ig)
+reminder(
+"This material is written using the `igraph` library. There are other libraries
+that provide the same basic functionality, but via different approaches,
+e.g. `networkx`."
+)
 
-# a) What happens if you change the order of the elements in the assignment
-# of `el` - i.e. swap c(1,2) and c(1,3)?  Or change c(1,2) to c(2,1)?
-# Note, the graph will almost certainly *look* different; check the
-# the edges and vertices in detail.
-# Answer:
-# You might see the graph change orientation, or perhaps have nodes in
-# the triangle be "flipped", but in fact these graphs are still isomorphic
-# see ?isomorphic for more detail
-el <- rbind(c(1,3),c(2,3),c(1,2),c(3,4))
-ig <- graph_from_edgelist(el, directed = FALSE)
-plot(ig)
+#' @section Motivation
+#'
+#' Network-based models (aka, graphs) are useful when the system you are trying
+#' to capture has lots of relationship structure that needs to be represented
+#' to accurately reproduce what is going on.
+#'
+#' In this warmup, we're going to show you how to build up two networks, both of
+#' people on a contact grid. In one, people are vaccinated at random; in the other, in
+#' a systematic way.
 
-# b) Change something about the pairs in `el` such that there will be a
-# meaningful change when you `plot(ig)`
-# Answer:
-# We can easily break up the triangle and instead create a square,
-# by having 1-4 linked instead of 1-3; there's another interesting
-# isomorphism question here - since the labels don't really matter
-# i.e. 1-2-3 triangle + 4 linked of any of the vertices is really the same
-# graph - how many different graphs can you create with 4 edges + 4 vertices?
-el <- rbind(c(1,4),c(2,3),c(1,2),c(3,4))
-ig <- graph_from_edgelist(el, directed = FALSE)
-plot(ig)
+# set some vertex and edge colors
+demo.cols <- c(
+  vaccinated = "blue", unvaccinated = "yellow",
+  transmissible = "grey", blocked = "transparent"
+)
 
-# While an edge provides a series of this vertex->that vertex, an adjacency matrix
-# has rows and columns correspond to vertices, and then 0 or 1 entry corresponding abscence
-# or presence of an edge
+# plot the two networks side-by-side (`network_warmup_vaccine...` - data
+# included in `MTM`, as are the `network_quickplot` and `patchwork_grid`
+# functions)
+list(list(
+  # random vaccination in a small, orderly population
+  "Random\nVaccination" = network_quickplot(
+    network_warmup_vaccine_random, values = demo.cols
+  ),
+  # targetted vaccination in same
+  "Targetted\nVaccination" = network_quickplot(
+    network_warmup_vaccine_ordered, values = demo.cols
+  )
+)) |> patchwork_grid()
 
-# to make the same plot we made earlier...
-# construct an empty matrix
-al <- matrix(0, nrow=4, ncol = 4)
-# connect the same vertices
-al[1,2] <- al[1,3] <- al[2,3] <- al[3,4] <- 1
-ig <- graph_from_adjacency_matrix(al, mode = "undirected")
-plot(ig)
-# advanced aside: we can also use slice assignment here:
-# el <- rbind(c(1,2),c(1,3),c(2,3),c(3,4))
-# al[el] <- 1 # the same as al[1,2] <- al[1,3] <- ... <- 1
+#' These plots highlight an important insight from the discussion portion of the
+#' session: that relationships can matter. Now let's work through the functions
+#' of the `igraph` library to build-up networks like these as well as those we
+#' will use in the later exercises.
+#'
+#' @aside The [network_quickplot()] function is what you will use throughout these
+#' practicals to visualize networks. This function streamlines supplying arguments
+#' to a several [ggplot2] functions and returns the resulting [ggplot2::ggplot]
+#' object. You may recall [patchwork_grid()] from previous sessions where you
+#' were laying out multiple plots for comparison; we use it here in the same way.
 
-# c) Create `al` such that it produces the same plot as the different `el` you created
-# for (b).
-# Answer: This remakes the square.
-al <- matrix(0, nrow=4, ncol = 4)
-al[1,2] <- al[1,4] <- al[2,3] <- al[3,4] <- 1
-ig <- graph_from_adjacency_matrix(al, mode = "undirected")
-plot(ig)
+#' @section Basic Structures
+#'
+#' the `igraph` library has several functions to create common network
+#' structures, including both deterministic ones (e.g. connected graphs and
+#' lattices) and probabilistic generators (e.g. Erdos-Renyi random graph).
+#'
+#' In `igraph`, the general convention is that the deterministic functions start
+#' with `make_...`, and the probabilistic generators start with `sample_...`
 
+# make several different kinds of graphs, at two sizesm L(ittle) = 9, B(ig) = 25
+igL <- make_full_graph(n = 9) |> add_layout_(with_graphopt())
+igB <- make_full_graph(n = 25) |> add_layout_(with_graphopt())
+igLgnp <- sample_gnp(n = 9, p = 0.2) |> set_graph_attr("layout", igL$layout)
+igBgnp <- sample_gnp(n = 25, p = 0.2) |> set_graph_attr("layout", igB$layout)
+igLl <- make_lattice(length = 3, dim = 2) |> add_layout_(on_grid())
+igBl <- make_lattice(length = 5, dim = 2) |> add_layout_(on_grid())
+# we use `add_layout()` for visualization purposes - it has no other effects
 
-# Advanced aside: you may have noticed the arguments `directed = FALSE` and
-# `mode = "undirected`.  We have only talked about undirected graphs so far,
-# but directed graphs can also be useful. In a directed graph,
-# edges go *from* one vertex *to* another.  Feel free to try making them now,
-# but for the rest of the exercises we will only be using *undirected* graphs
+# feel free to inspect those objects if you like, but you can get the overall
+# point by plotting them alongside each other:
+list(list(
+  "N=9 Clique"  = network_quickplot(igL, simple = TRUE),
+  "N=25 Clique" = network_quickplot(igB, simple = TRUE)
+), list(
+  "N=9 Random"  = network_quickplot(igLgnp, simple = TRUE),
+  "N=25 Random" = network_quickplot(igBgnp, simple = TRUE)
+), list(
+  "N=9 Lattice"  = network_quickplot(igLl, simple = TRUE),
+  "N=25 Lattice" = network_quickplot(igBl, simple = TRUE)
+)) |> patchwork_grid()
 
-# directed options
-el <- rbind(c(1,2),c(1,3),c(2,3),c(3,4))
-ig <- graph_from_edgelist(el)
-plot(ig)
-al <- matrix(0, nrow=4, ncol = 4)
-al[el] <- 1
-ig <- graph_from_adjacency_matrix(al)
-plot(ig)
+#' @question How would you describe the difference between the graphs created by
+#' `make_full_graph()` vs. `sample_gnp()` vs. `make_lattice()`?
+#'
+#' @answer The `make_...` generators produce graphs with fixed properties; all
+#' vertices connected for `make_full_graph()` and vertices connected in a grid
+#' for `make_lattice()` (a typical 2D one for the basic arguments, or more
+#' complicated grids if you fiddled around with `dim=` arguments).
+#' `sample_gnp()` produces a random set of edges amongst the vertices. If you
+#' look at another person's plots, they will likely have different connections.
+#' (You may also notice slightly different layouts, particularly for the "B"
+#' networks.)
+#'
+#' @question Of the three generators, `make_full_graph()`, `make_lattice()`, and
+#' `sample_gnp()`, which do you think is behind the plots we first looked at?
+#'
+#' @answer The example networks above are made with `make_lattice()`, with some
+#' other modifications. In the later exercises, we will use `make_full_graph()`,
+#' and then make modifications.
+#'
+#' @hint look at `?make_full_graph`, `?make_lattice`, and `?sample_gnp`.
+#'
+#' @aside Note that we are adding layouts to the plots above. We do that to
+#' ensure getting particular plots; without them, each plot of an igraph object
+#' gives different arrangements of vertices and edges. For example:
 
-# You can of course store and read in graphs in various formats
-write_graph(ig, file = "example.csv", format = "edgelist")
-# though pay attention to the directed-ness when reading in from edgelists
-ig2 <- read_graph(file = "example.csv", format = "edgelist")
-ig3 <- read_graph(file = "example.csv", format = "edgelist", directed = FALSE)
-# d) Try out the "edgelist", "graphml", and "dot" formats, so you can recognize which is which.
-# Describe the differences.
-# Answer:
-write_graph(ig, file = "example_d.gml", format = "gml")
-write_graph(as.undirected(ig), file = "example_u.gml", format = "gml")
-ig4 <- read_graph(file = "example_d.gml", format = "gml")
-ig5 <- read_graph(file = "example_u.gml", format = "gml")
-write_graph(ig, file = "example_d.dot", format = "dot")
-write_graph(as.undirected(ig), file = "example_u.dot", format = "dot")
-# note: these don't actually work! igraph only supports exporting in dot format
-# not reading it back in
-ig6 <- read_graph(file = "example_d.dot", format = "dot")
-ig7 <- read_graph(file = "example_u.dot", format = "dot")
+list(list(
+  "Original" = network_quickplot(igL, simple = TRUE),
+  "...Another" = network_quickplot(make_full_graph(n = 9), simple = TRUE),
+  "& Another" = network_quickplot(make_full_graph(n = 9), simple = TRUE)
+)) |> patchwork_grid()
 
-########## 2 - Better ways of Creating iGraph objects  ########################
-# igraph also provides constructors for archetypal graphs:
-ig.star <- make_star(5, mode = "undirected")
-plot(ig.star)
-ig.ring <- make_ring(5)
-plot(ig.ring)
+#' @section Modifying Networks, part 1
+#'
+#' In the previous section, we built some foundational networks using the tools
+#' in [igraph]. But those had none of the attributes that we discussed as part
+#' of the module or that can be seen as part of the initial networks we looked
+#' at in this practical.
+#'
+#' To make those networks, we need to use some of the other [igraph] tools for
+#' adding and deleting vertices and edges, and for assigning properties to edges
+#' and vertices.
 
-# and you can do things like add those together:
-ig.combo <- ig.star + ig.ring
+# delete every other edge on the lattice
+igBl <- make_lattice(length = 5, dim = 2) |> add_layout_(on_grid())
+igBlmod <- igBl |> delete_edges(1:(ecount(igBl)/2)*2)
+# repeated definition of igBl, in case it got deleted/changed/etc
 
-# a) What do you expect `ig.combo` looks like plotted?
-# Answer:
-# Either it was going to merge the graphs, or have two disconnected
-# components; in this case it's disconnected components
+list(list(
+  "N=25 Lattice" = network_quickplot(igBl, simple = TRUE),
+  "N=25 Lattice--" = network_quickplot(igBlmod, simple = TRUE)
+)) |> patchwork_grid() & geom_text(
+  aes(
+    x=(x1+x2)/2, y=(y1+y2)/2, label = eid
+  ), data = network_edge_data(igBl)
+)
 
+#' @question This is starting to look like the initial networks we plotted.
+#' What collection of edges could we delete to isolate the diagonal set of
+#' vertices?
+#'
+#' @answer There's a clear pattern of adding `11` to `c(1,2,4,10)` until hitting
+#' the top row of vertices, so could do roughly
+#' `del <- c(1,2,4,10) + 11*rep(0:3, each=4); del[15:16] <- c(36, 40)`
+#'
+#' @aside If you wanted to make this work for any size lattice, you have to
+#' consider how that the addition increment changes & how to deal with the
+#' "last row" switch. For example, consider a slightly larger version of the
+#' problem:
 
-# Let's try it:
-plot(ig.combo)
+igBBl <- make_lattice(length = 7, dim = 2) |> add_layout_(on_grid())
+igBBlmod <- igBBl |> delete_edges(1:(ecount(igBBl)/2)*2)
+list(list(
+  "N=49 Lattice" = network_quickplot(igBBl, simple = TRUE),
+  "N=49 Lattice--" = network_quickplot(igBBlmod, simple = TRUE)
+)) |> patchwork_grid() & geom_text(
+  aes(x=(x1+x2)/2, y=(y1+y2)/2, label = eid), data = network_edge_data(igBBl)
+)
 
-# that plot is probably one of (1) exactly what you expected and (2) the opposite
-# To get the other (whichever it was for you), you can do something like:
-ig.combo <- graph.union(ig.star, ig.ring)
-plot(ig.combo)
+#' In a later section, we'll use some of the other [igraph] capabilities to
+#' do this more cleverly. For now, let's consider the tools to add vertices
+#' and edges to a graph.
 
-# b) Try some of the other `make_...` functions.
-# What are `make_lattice`, `make_full_graph`, `make_tree` graphs like?
-# Which one do you think will be relevant to the Reed-Frost SIR model?
-# Answer:
-igl2 <- make_lattice(c(1, 4, 4))
-plot(igl2)
-igl3 <- make_lattice(c(3, 4, 4))
-plot(igl3)
-igfg <- make_full_graph(8)
-plot(igfg)
-igtree <- make_tree(16)
-plot(igtree)
+# adding edges ...
+iglstar <- make_star(9, mode = "undirected") |> add_layout_(as_star())
+# can be done by naming specific vertices
+iglstarmod <- iglstar |> add_edges(c(c(2,4), c(3,5), c(6,8), c(7,9)))
 
-# Finally, you can create new graphs using `-` and `+` operators;
-# note that these operations do *not* change the underlying graph
-# unless you reassign the results
-ig.combo - edge("1|4")
-plot(ig.combo)
-ig.combo <- ig.combo - edge("1|4")
-plot(ig.combo)
+list(list(
+  "N=9 Star" = network_quickplot(iglstar, simple = TRUE),
+  "N=9 Star+edges" = network_quickplot(iglstarmod, simple = TRUE)
+)) |> patchwork_grid() & geom_text(
+  aes(x=x + .1, y=y, label = vid), data = network_vertex_data(iglstar |> as.data.table())
+)
 
-########## 3 - Manipulating iGraph objects  ###################################
-# igraph also a variety of ways to interact with edges and vertices
-
-ig.ref <- make_tree(21, mode = "undirected")
-plot(ig.ref)
-
-# igraph provides two useful ways to interact with edges and vertices:
-V(ig.ref)
-E(ig.ref)
-
-# a) How many vertices are there? How many edges?
-# Answer:
-# 20 vertices
-# 19 edges
-
-
-# You can also use `E` and `V` to assign properties
-E(ig.ref)$color <- c("red", "green")
-V(ig.ref)$color <- "blue"
-plot(ig.ref)
-V(ig.ref)$color[c(5,10,15,20)] <- "purple"
-plot(ig.ref)
-
-# There are several specific properties associated with plotting; `?plot.igraph`
-# for more detail.  But these properties can be anything, such as vaccine status:
-
-ig.grid <- make_lattice(length=10, dim=2)
-V(ig.grid)$color <- "yellow"
-plot(ig.grid)
-V(ig.grid)$vax_status <- "unvaccinated"
-V(ig.grid)$vax_status[seq(1,100,by=11)] <- "vaccinated"
-
-# and those properties can be used to filter the graph for other purposes:
-
-V(ig.grid)[vax_status == "vaccinated"]$color <- "blue"
-
-# b) The previous `plot(ig.grid)` appeared as a yellow square; what will
-# setting the 1, 12, 23, ..., 100 vertices to blue do? (i.e, V(ig.grid)[vax_status == "vaccinated"]$color <- "blue")
-# Answer:
-# it creates a diagonal partition
-
-# Check your answer
-plot(ig.grid)
-
-# We can use a similar approach for edge properties:
-E(ig.grid)$transmissible <- TRUE # ...initially make all edges transmissible
-# there are special functions for selecting edges based on vertices and
-# vice versa; use `.inc` (one such function) to make the connections to vaccinated individuals
-# non-transmissible
-E(ig.grid)[.inc( # get incident edges to...
-  V(ig.grid)[vax_status == "vaccinated"] # all vertices with a particular vax_status
-)]$transmissible <- FALSE # and set their property `transmissible` to FALSE
-
-# c) you will probably need to use these special functions in the next
-# practicals.  Read ?`igraph-es-indexing` and ?`igraph-vs-indexing`.
-# List the special filtering functions with a short description of what
-# they do.
-# Answer:
-# For edges, there is .inc, .from, and .to which let you filter edges based on associated vertices
-# .inc matches edges associated with vertices at either end, .from only vertices that are edge sources,
-# .to only vertices the edge targets
-# the %--%, %->%, and %<-% operators do the same tasks, but for matching both end vertices at once
-# For vertices, there's also .inc, .from, and .to functions, but they work by taking a series of edges
-# and returning the vertices (that those edges are incident on, from, or to, respectively)
-# vertices can also be looked up by their vertex neighbors with .nei (and specializations .innei, .outnei).
-
-# Read these outputs to see how `ig.grid` has changed
-E(ig.grid)$transmissible
-E(ig.grid)[transmissible == FALSE]
-V(ig.grid)[vax_status == "vaccinated"]
-V(ig.grid)[vax_status == "unvaccinated"]
-
-# Let's use these properties to show how our model targetted vaccine
-# campaign has partitioned the network
-E(ig.grid)$color <- "red"
-E(ig.grid)[transmissible == FALSE]$color <- "blue"
-plot(ig.grid)
-
-# recalling to earlier in warmup, we could also simply delete edges
-# based on these properties:
-plot(ig.grid - E(ig.grid)[transmissible == FALSE])
+#' @question What are some other approaches to add edges in [igraph]?
+#'
+#' @answer You can use `+` with another [igraph]-like entity, either a whole
+#' network or pieces created with [igraph::vertex()], [igraph::vertices()],
+#' [igraph::edge()], or [igraph::edges()].
+#'
+#' @hint check `?add_edges`
