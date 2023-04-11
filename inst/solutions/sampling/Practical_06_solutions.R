c499731f0a33c79caddb00f867f24db1f754aeec
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-04-11 15:38:47 +0100

    add LHS section

--- inst/solutions/sampling/Practical_06_solutions.R
+++ inst/solutions/sampling/Practical_06_solutions.R
@@ -47,16 +47,16 @@ plot(1/gamma.vec, max.prevalence, type = "b",
 
 
 ## Now try to increase the resolution of gamma to get a better idea of the relationship
+# but remember to clear max.prevalence first!
 
 # Replace gamma.vec = seq(0.1, 1.0, by = 0.1) with
 inf.duration = 1:10
 gamma.vec = 1/inf.duration
-# but remember to clear max.prevalence first!
 
 # (d) Describe in words the qualitative relationship
 # Answer: There is no epidemic until the infectiousness duration is >2 (R0>1)
 # after that there is a linear increase in the maximum prevalence until gamma = 6,
-# then there is a diminishing increase in maximum prevlance
+# then there is a diminishing increase in maximum prevalence
 
 ###################### (2) MONTE CARLO SAMPLING ######################
 
@@ -103,7 +103,7 @@ for (sample_index in 1:dim(max.prevalence)[1]) {
 # Take a look at max.prevalence by using the 'head() function
 head(max.prevalence)
 # (e) How have we saved the output?
-# Answer: using 'long' formatting -- see the next practical?
+# Answer: using 'long' formatting -- see the ggplot pre-course material
 
 # Now plot this output using the R function MTM::sampling_MC_plot()
 sampling_MC_plot(max.prevalence)
@@ -113,19 +113,88 @@ sampling_MC_plot(max.prevalence)
 # However, the uncertainty in R0 has a larger impact on the maximum prevalence than infectious duration
 # In fact, until the infectiousness duration decreases below 1/0.3 (3d), this parameter doesn't affect prevalence
 
-# (g) Why does the plot look different to the one you plotted previously?
-# Final exercise:  We have used a combination of scripts and function to complete this plot.
-# (g) Write out the description of how we could plot this output ONLY using functions (optimal)
-# Answer: Write out the main components of the code:
- # 1. Pick which parameters and how they will be sampled / sequenced
- # 2. Generate an outcome measure from the parameter values
- # 3. Plot each outcome measure as a function of the parameter values
-# (h) Next add to this outline by splitting these descriptions into functions
-# MAIN FUNCTION
-  # > calls SAMPLING FUNCTION (output set of samples / sequences of parameters)
-  # > calls ODE SOLVE FUNCTION (input output from SAMPLING, output variable of interest)
-  # > calls PLOT FUNCTION (input= output variable from ODE solve function; no output)
-# These 4 components could be in their own source files. Sampling, ODE solve and Plot might all call other functions (located in the same file)
 
-# (h) Translate this outline to actual code - notice how there is a trade-off between making your code
- # quick to write and making it generalisable to other situations
+########################### (3) LHS vs MONTE CARLO SAMPLING ##############################
+
+## First let's load in the library we'll need for later
+
+library(lhs)
+
+# We're going to first sample directly from a full distribution uniform distribution from 0 to 1.
+# How many samples will we need to take?
+
+# Let's try a few options and see how well they do
+par(mfrow=c(3,2))
+hist(rnorm(10))
+hist(rnorm(100))
+hist(rnorm(1000))
+hist(rnorm(10000))
+hist(rnorm(20000))
+
+
+# Now let's plot the sample sizes against the variance of the sample distribution
+
+plot(c(10,100,1000,10000,50000,100000),
+     c(var(rnorm(10)), var(rnorm(100)), var(rnorm(1000)), var(rnorm(10000)), var(rnorm(50000)), var(rnorm(100000))),
+     ylab = "variance", main = "Variance of sampled normal")
+abline(h = 1)
+
+
+
+### Let's now use 100 samples to see the difference between a Monte Carlo sampling and a LHS sampling approach
+# Pick some small number of samples
+n <- 100
+
+# First we're going to sample 100 times from a random sample
+mc_unif <- runif(n)
+# 100 lh samples across 1 parameter
+latin_unif <- randomLHS(n, 1)
+
+#plot these two distribution
+dev.off()
+par(mfrow=c(3,2))
+hist(mc_unif)
+hist(latin_unif)
+
+# You can see how the Latin Hypercube does a great job of sampling evenly across the distribution
+
+# Let's now sample from a Normal distribution using a random monte carlo sample across the whole distribution
+mc_norm <- rnorm(n, mean = 0, sd = 1)
+
+# How do we sample using an LHS?
+# We use the previous numbers generated from the uniform LHS to draw samples from the Normal using the Inverse Cumulative Sampling
+latin_norm <- qnorm(latin_unif, mean = 0, sd = 1)
+
+
+#plot these two normal distributions
+hist(mc_norm, xlim = c(-6,6))
+hist(latin_norm, xlim = c(-6,6))
+
+# the latin hypercube sample looks much better!
+# Why does this work?
+
+# first let's look at the norm probability distibution
+x <- seq(-6,6, by =0.1) #random variable X
+normdens <- dnorm(x, mean = 0, sd = 1) # prob distribution, f(X)
+normcumul <- pnorm(x, mean = 0, sd = 1) # cumulative distribution, F(X)
+plot(x, normdens, "l")
+plot(x, normcumul, "l")
+
+# Most of the density is in the middle range of values (-1 to 1).
+# So we want a method to sample from this more often than the other areas in the distribution.
+# Specifically, we want to sample values from X proportionally to the probability of those values occuring
+# Let's generate some samples between 0-1. These can be values on our Y-axis. Then, if we ask what is the value of the cumulative distribution that corresponds to these uniform values we are taking the inverse
+
+# for illustration let's just choose 10 points
+ex_latin <- randomLHS(10, 1)
+# which X values are given by using these as the Y value (denoted by "X"s)?
+abline(h = ex_latin, col = "red")
+points(qnorm(ex_latin, mean = 0, sd = 1), y=rep(0,10), pc = "x")
+# You can see that the samples are clustered around the middle:
+# in areas of X with higher density, the gradient of the cumluative distribution (F(X)) will be very steep, causing more values between 0 and 1 to map to this range of X with high density
+# That is, F^{-1}(R) = X where R is a uniform random number between 0 and 1.
+
+# So, you can sample from any distribution whose cumluative function is 'invertable' by plugging in uniform random numbers to the inverse cumulative function of your new distribution
+# for more information check out: https://en.wikipedia.org/wiki/Inverse_transform_sampling
+
+# Likewise, to perform LHS on a uni- or mulitvariate non-uniform distribution, we can transform our LHS samples from a uniform distribution as above.
