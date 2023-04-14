48f48d846594bd58f671720dff0c9fb88d00a4e4
nicholasdavies <n.g.davies@gmail.com>
2023-04-14 12:27:46 +0100

    Updated individual-based material for 2023

--- inst/solutions/09_StochasticIBM/Practical_01.R
+++ inst/solutions/09_StochasticIBM/Practical_01.R
@@ -1,108 +1,92 @@
-# A model of sensitive and resistant Streptococcus pneumoniae competition in the nasopharynx
-library(ggplot2) # For plotting
+# SOLUTIONS: Individual-based SARS-CoV-2 transmission model, practical 1
+library(ggplot2)
 
-# A. Model parameters: events
-g <- 0.02     # "Germ size" of a transmitted strain [0.02]
-m <- 20.0     # Growth constant of S. pneumoniae within host [20.0]
-b <- 0.05     # Within-host growth "benefit" of sensitive strain [0.05]
 
-# B. Model parameters: granularity of simulation
-dt <- 1/30   # Length of each time step in months [1/30]
-years <- 1   # Number of years to simulate [1]
+## Model parameters
+beta <- 0.5      # Transmission parameter
+delta <- 1/2.5   # Rate of transitioning out of latent state
+gamma <- 1/5     # Rate of transitioning out of infectious state
+omega <- 1/180   # Rate of waning immunity
 
-##### The total number of time steps to run the simulation.
-##### Hint: this can be calculated based on years, dt, and the number of months per year.
-steps <- (12 * years)/dt
+dt <- 1          # Time step of simulation (1 day)
+days <- 365      # Duration of simulation (365 days)
+steps <- days/dt # Total number of time steps
+n <- 1000        # Population size
 
-# C. Events
-# Here, we define, in functional form, all the events that can happen to a host.
-# Each function takes a vector (s, r) of the host's sensitive and resistant strain carriage
-# and evaluates to a vector of the same format.
 
-# Exposure to sensitive strain
-ExposeToS <- function(h) {
-    list(s = h$s + g, r = h$r)
-}
+## Data frame to store simulation results
+results <- data.frame(ts = 1:steps, S = 0, E = 0, I = 0, R = 0)
 
-##### Fill in the other 4 needed event functions below:
-ExposeToR <- function(h) {
-    list(s = h$s, r = h$r + g)
-}
 
-Clearance <- function(h) {
-    list(s = 0, r = 0)
-}
+## Initialize simulation
 
-Treatment <- function(h) {
-    list(s = 0, r = h$r)
-}
+# Set the seed for the pseudorandom number generator, for reproducibility
+set.seed(12345)
 
-Growth <- function(h) {
-    ds_dt <- h$s * m * (1 + b - h$s - h$r)
-    dr_dt <- h$r * m * (1 - h$s - h$r)
-    list(s = h$s + ds_dt * dt, r = h$r + dr_dt * dt)
-}
+# Since this is an individual-based model, we track the properties of all n
+# individuals in the simulation. One kind of property we can track is a state,
+# such as S (susceptible), E (exposed), I (infectious), or R (recovered). We
+# will store each individual's state as a string, either "S", "E", "I", or "R".
 
-##### D. Test of events
-##### Do each of these events do what you expect them to do?
-test_host <- list(s = 0.5, r = 0.5)
-test_host
-ExposeToS(test_host)
-ExposeToR(test_host)
-Clearance(test_host)
-Treatment(test_host)
-Growth(test_host)
+state <- rep("S", n)   # Each individual's state: start with all susceptible
+state[1:10] <- "E"     # Start 10 individuals in the "exposed" state
 
-##### E. The simulation for dynamics within an individual host
-##### We define our host as a list with two properties, s and r,
-##### for sensitive and resistant carriage respectively.
-host <- list(s = 0, r = 0)
 
-##### We will also want to store the results of our simulation at
-##### regular time steps, which we can use a data frame to do.
-##### Any parameters passed to data.frame of length 1 (like s and
-##### r below) are automatically expanded to be the length of the
-##### longest parameter (in this case, time).
-results <- data.frame(time = 0:steps * dt, s = 0, r = 0)
+## Run simulation
 
-##### Before the simulation begins, store the host's properties
-##### for the first time step.
-results$s[1] <- host$s
-results$r[1] <- host$r
+# We'll use the built-in function txtProgressBar to track the simulation's progress.
+# Really helps for planning coffee breaks! It needs to know the minimum and maximum
+# values to expect, and style = 3 tells it to report the percentage complete.
+bar <- txtProgressBar(min = 1, max = steps, style = 3)
 
 # Loop over each time step . . .
 for (ts in 1:steps)
 {
-    ##### Basically, we just want to see if a sequence of events in time
-    ##### produces the expected output. So fill in some events below to
-    ##### see if it all makes sense.
-    ##### There is no single "correct" sequence here, of course!
-    if (ts == 1)     host <- ExposeToR(host)
-    if (ts == 50)    host <- ExposeToS(host)
-    if (ts == 100)   host <- Clearance(host)
-    if (ts == 150)   host <- ExposeToS(host)
-    if (ts == 150)   host <- ExposeToR(host)
-    if (ts == 300)   host <- Treatment(host)
-    
-    ##### Each time step, the host should be subjected to growth and competition.
-    host <- Growth(host)
+    # Calculate the force of infection
+    lambda <- beta * sum(state == "I") / n
 
-    ##### Store simulation results.
-    results$s[ts + 1] <- host$s
-    results$r[ts + 1] <- host$r
+    # Loop through each individual . . .
+    for (i in 1:n)
+    {
+        if (state[i] == "S") {
+            # Transition S -> E (infection) at rate lambda
+            if (runif(1) < 1 - exp(-lambda * dt)) {
+                state[i] <- "E"
+            }
+        } else if (state[i] == "E") {
+            # Transition E -> I (latent to infectious) at rate delta
+            if (runif(1) < 1 - exp(-delta * dt)) {
+                state[i] <- "I"
+            }
+        } else if (state[i] == "I") {
+            # Transition I -> R (infectious to recovered) at rate gamma
+            if (runif(1) < 1 - exp(-gamma * dt)) {
+                state[i] <- "R"
+            }
+        } else if (state[i] == "R") {
+            # Transition R -> S (waning of immunity) at rate omega
+            if (runif(1) < 1 - exp(-omega * dt)) {
+                state[i] <- "S"
+            }
+        }
+    }
+    
+    # Save population state for this time step
+    results[ts, "S"] <- sum(state == "S")
+    results[ts, "E"] <- sum(state == "E")
+    results[ts, "I"] <- sum(state == "I")
+    results[ts, "R"] <- sum(state == "R")
+    
+    # Update progress bar; close progress bar if we are finished
+    setTxtProgressBar(bar, ts)
+    if (ts == steps) {
+        close(bar)
+    }
 }
 
-##### F. Plot simulation results
-##### Try using ggplot on the "results" data frame to visualise the dynamics within
-##### your host from above.
-
-##### Lots of ways of showing the results -- here are a couple.
+## Plot simulation results
 ggplot(results) + 
-    geom_line(aes(x = time, y = r), colour = "red") + 
-    geom_line(aes(x = time, y = s), colour = "blue") +
-    labs(x = "Time (months)", y = "Carriage density")
-
-ggplot(results) +
-    geom_ribbon(aes(x = time, ymin = 0, ymax = r), fill = "red") + 
-    geom_ribbon(aes(x = time, ymin = r, ymax = r + s), fill = "blue") +
-    labs(x = "Time (months)", y = "Carriage density")
+    geom_line(aes(x = ts, y = S, colour = "S")) + 
+    geom_line(aes(x = ts, y = E, colour = "E")) + 
+    geom_line(aes(x = ts, y = I, colour = "I")) + 
+    geom_line(aes(x = ts, y = R, colour = "R"))
