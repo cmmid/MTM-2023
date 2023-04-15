5159359c8e434a2c8dcd28cbfe467b98a3e0571b
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-04-15 10:29:42 +0100

    09-02 & 03 parallel linting

--- inst/scripts/09_StochasticIBM/Practical_02.R
+++ inst/scripts/09_StochasticIBM/Practical_02.R
@@ -16,36 +16,31 @@ n <- 1000        # Population size
 
 
 ## Some helper functions
-# Calculates infectiousness as a function of state and age: zero if state is 
+# Calculates infectiousness as a function of state and age: zero if state is
 # not "I"; nonzero if state is "I", and slightly decreasing with age
-infectiousness = function(state, age)
-{
+infectiousness <- function(state, age) {
     ifelse(state == "I", 1.25 - age / 160, 0)
 }
 
 # Calculates susceptibility of individuals with antibody level(s) ab
-susceptibility = function(ab)
-{
+susceptibility <- function(ab) {
     pnorm(ab, 5, 1)
 }
 
-# Generates n random delays from the latent-period distribution 
+# Generates n random delays from the latent-period distribution
 # (approximately 2 days, on average)
-latent_delay = function(n)
-{
+latent_delay <- function(n) {
     rlnorm(n, meanlog = 0.5, sdlog = 0.6)
 }
 
-# Generates n random delays from the infectious-period distribution 
+# Generates n random delays from the infectious-period distribution
 # (approximately 5 days, on average)
-infectious_delay = function(n)
-{
+infectious_delay <- function(n) {
     rlnorm(n, meanlog = 1.5, sdlog = 0.5)
 }
 
 # Generates n random increments to antibody levels following recovery
-ab_increment = function(n)
-{
+ab_increment <- function(n) {
     rnorm(n, mean = 12, sd = 2)
 }
 
@@ -70,17 +65,15 @@ state[1:10] <- "E"     # Start 10 individuals in the "exposed" state
 bar <- txtProgressBar(min = 1, max = steps, style = 3)
 
 # Loop over each time step . . .
-for (ts in 1:steps)
-{
+for (ts in 1:steps) {
     # Calculate the force of infection
     lambda <- beta * sum(state == "I") / n + iota
 
     # Loop through each host . . .
-    for (i in 1:n)
-    {
+    for (i in 1:n) {
         # Update individual i's non-state variables
         # . . .
-        
+
         # Update individual i's state
         if (state[i] == "S") {
             # Transition S -> E (infection) at rate lambda
@@ -104,13 +97,13 @@ for (ts in 1:steps)
             }
         }
     }
-    
+
     # Save population state for this time step
     results[ts, "S"] <- sum(state == "S")
     results[ts, "E"] <- sum(state == "E")
     results[ts, "I"] <- sum(state == "I")
     results[ts, "R"] <- sum(state == "R")
-    
+
     # Update progress bar; close progress bar if we are finished
     setTxtProgressBar(bar, ts)
     if (ts == steps) {
@@ -119,8 +112,8 @@ for (ts in 1:steps)
 }
 
 ## Plot simulation results
-ggplot(results) + 
-    geom_line(aes(x = ts, y = S, colour = "S")) + 
-    geom_line(aes(x = ts, y = E, colour = "E")) + 
-    geom_line(aes(x = ts, y = I, colour = "I")) + 
-    geom_line(aes(x = ts, y = R, colour = "R"))
+ggplot(results) +
+  geom_line(aes(x = ts, y = S, colour = "S")) +
+  geom_line(aes(x = ts, y = E, colour = "E")) +
+  geom_line(aes(x = ts, y = I, colour = "I")) +
+  geom_line(aes(x = ts, y = R, colour = "R"))
